80bb7baaf9d14e83c15281d8f5bb7370365274ea
maxiemar <denishicmihail@gmail.com>
2021-05-06 22:07:05 +0300

    Add comparators

--- src/by/jwd/task6/fleet/Airplane.java
+++ src/by/jwd/task6/fleet/Airplane.java
@@ -2,9 +2,85 @@ package by.jwd.task6.fleet;
 
 import by.jwd.task6.util.HashUtil;
 
+import java.util.Comparator;
 import java.util.Objects;
 
 public class Airplane implements RegisteredAircraft {
+
+    /**
+     * Aircraft flight performance comparators.
+     */
+    public final static Comparator<Airplane> CRUISING_SPEED_COMPARATOR =
+            Comparator.comparing(airplane -> airplane.getModel().getAircraftPerformance(),
+                                 AircraftPerformance.CRUISING_SPEED_COMPARATOR);
+    public final static Comparator<Airplane> FUEL_CONSUMPTION_COMPARATOR =
+            Comparator.comparing(airplane -> airplane.getModel().getAircraftPerformance(),
+                                 AircraftPerformance.FUEL_CONSUMPTION_COMPARATOR);
+    public final static Comparator<Airplane> MAX_ATTITUDE_COMPARATOR =
+            Comparator.comparing(airplane -> airplane.getModel().getAircraftPerformance(),
+                                 AircraftPerformance.MAX_ATTITUDE_COMPARATOR);
+    public final static Comparator<Airplane> MAX_RANGE_COMPARATOR =
+            Comparator.comparing(airplane -> airplane.getModel().getAircraftPerformance(),
+                                 AircraftPerformance.RANGE_COMPARATOR);
+
+    /**
+     * Aircraft size comparators.
+     */
+    public final static Comparator<Airplane> FUSELAGE_LENGTH_COMPARATOR =
+            Comparator.comparing(airplane -> airplane.getModel().getAircraftSize(),
+                                 AircraftSize.FUSELAGE_LENGTH_COMPARATOR);
+    public final static Comparator<Airplane> FUSELAGE_WIDTH_COMPARATOR =
+            Comparator.comparing(airplane -> airplane.getModel().getAircraftSize(),
+                                 AircraftSize.FUSELAGE_WIDTH_COMPARATOR);
+    public final static Comparator<Airplane> OVERALL_HEIGHT_COMPARATOR =
+            Comparator.comparing(airplane -> airplane.getModel().getAircraftSize(),
+                                 AircraftSize.OVERALL_HEIGHT_COMPARATOR);
+    public final static Comparator<Airplane> OVERALL_LENGTH_COMPARATOR =
+            Comparator.comparing(airplane -> airplane.getModel().getAircraftSize(),
+                                 AircraftSize.OVERALL_LENGTH_COMPARATOR);
+    public final static Comparator<Airplane> OVERALL_WIDTH_COMPARATOR =
+            Comparator.comparing(airplane -> airplane.getModel().getAircraftSize(),
+                                 AircraftSize.OVERALL_WIDTH_COMPARATOR);
+
+    /**
+     * Aircraft weight comparators.
+     */
+    public final static Comparator<Airplane> EMPTY_WEIGHT_COMPARATOR =
+            Comparator.comparing(airplane -> airplane.getModel().getAircraftWeight(),
+                                 AircraftWeight.EMPTY_WEIGHT_COMPARATOR);
+    public final static Comparator<Airplane> FUEL_CAPACITY_COMPARATOR =
+            Comparator.comparing(airplane -> airplane.getModel().getAircraftWeight(),
+                                 AircraftWeight.FUEL_CAPACITY_COMPARATOR);
+    public final static Comparator<Airplane> LANDING_WEIGHT_COMPARATOR =
+            Comparator.comparing(airplane -> airplane.getModel().getAircraftWeight(),
+                                 AircraftWeight.LANDING_WEIGHT_COMPARATOR);
+    public final static Comparator<Airplane> TAKEOFF_WEIGHT_COMPARATOR =
+            Comparator.comparing(airplane -> airplane.getModel().getAircraftWeight(),
+                                 AircraftWeight.TAKEOFF_WEIGHT_COMPARATOR);
+
+    /**
+     * Aircraft model name comparator.
+     */
+    public final static Comparator<Airplane> MODEL_NAME_COMPARATOR =
+            Comparator.comparing(Airplane::getModel, AirplaneModel.MODEL_NAME_COMPARATOR);
+
+    /**
+     * Aircraft manufacturer comparator.
+     */
+    public final static Comparator<Airplane> MANUFACTURER_COMPARATOR =
+            Comparator.comparing(Airplane::getModel, AirplaneModel.MANUFACTURER_COMPARATOR);
+
+    /**
+     * Airplane model landing distance comparator.
+     */
+    public static final Comparator<AirplaneModel> LANDING_DISTANCE_COMPARATOR =
+            Comparator.comparing(AirplaneModel::getLandingDistance);
+
+    /**
+     * Airplane model takeoff distance comparator.
+     */
+    public static final Comparator<AirplaneModel> TAKEOFF_DISTANCE_COMPARATOR =
+            Comparator.comparing(AirplaneModel::getTakeoffDistance);
 
     private AirplaneModel model;
     private String notes;
--- src/by/jwd/task6/fleet/RegisteredAircraft.java
+++ src/by/jwd/task6/fleet/RegisteredAircraft.java
@@ -1,6 +1,20 @@
 package by.jwd.task6.fleet;
 
+import java.util.Comparator;
+
 public interface RegisteredAircraft {
+
+    /**
+     * Registered aircraft notes comparator.
+     */
+    Comparator<RegisteredAircraft> NOTES_COMPARATOR =
+            Comparator.comparing(RegisteredAircraft::getNotes);
+
+    /**
+     * Registered aircraft registration code comparator.
+     */
+    Comparator<RegisteredAircraft> REGISTRATION_CODE_COMPARATOR =
+            Comparator.comparing(RegisteredAircraft::getRegistrationCode);
 
     String NULL_MODEL_MESSAGE = "Model cannot be null";
     String NULL_REGISTRATION_CODE_MESSAGE = "Registration code cannot be null";
